269e4390396fe63506a059c4b029dde661d899eb
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-17 21:22:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
 # your code goes here
+require 'pry'
+
+def begins_with_r(elem)
+  elem.all? {|word| word.start_with?("r".downcase)}
+#    binding.pry
+end
+
+def contain_a(elem)
+  elem.map do |word|
+    if word.include?("a")
+      word
+    else
+      nil
+    end
+  end.compact
+end
+
+def first_wa(elem)
+  elem.find do |word|
+    word.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings(elem)
+  elem.map do |word|
+    if word.is_a? String
+      word
+    else
+      nil
+    end
+  end.compact
+end
+
+def count_elements(elem)
+  full = []
+  elem.each do |data|
+    count_hash = {:count => data.count}
+     #binding.pry
+    full << count_hash.merge(data)
+  end
+  binding.pry
+  full
+end
+
+def merge_data(keys, data)
+end
+
+def find_cool(cool)
+end
+
+def organize_schools(schools)
+end
